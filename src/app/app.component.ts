eab8551391a50880a902fb92ffb76936a8193173
Sharan Ainapurapu <ainapurapu.sharan@siriuscom.com>
2020-04-07 18:43:21 +0530

    Final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { single, single2 } from './data';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-app1';
+  single: any[];
+  multi: any[];
+
+  view: any[] = [700, 400];
+
+  // options
+  showXAxis = true;
+  showYAxis = true;
+  gradient = false;
+  showLegend = true;
+  showXAxisLabel = true;
+  xAxisLabel = 'Country';
+  showYAxisLabel = true;
+  yAxisLabel = 'Population';
+
+  colorScheme = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+  };
+
+  constructor() {
+    Object.assign(this, { single })
+    Object.assign(this, { single2 });
+  }
+
+  onSelect(event) {
+    console.log(event);
+  }
+
+  single2: any[];
+  view2: any[] = [700, 400];
+
+  // options
+  gradient2: boolean = true;
+  showLegend2: boolean = true;
+  showLabels2: boolean = true;
+  isDoughnut2: boolean = false;
+
+  colorScheme2 = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+  };
+
+  onSelect2(data): void {
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  }
+
+  onActivate2(data): void {
+    console.log('Activate', JSON.parse(JSON.stringify(data)));
+  }
+
+  onDeactivate2(data): void {
+    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
+  }
+
 }
